37c9bdc0880620dc1c1cd7da968eb2168bf26783
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-11 18:59:02 +0300

    Implemented try and exception in loading audio files

--- scripts/app.py
+++ scripts/app.py
@@ -19,8 +19,11 @@ import logging
 import logging.handlers
 
 path_to_wave_files = "../speech_recognition/data/AMHARIC/test/wav/01_d501021.wav"
-MODEL_URL = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm"
+MODEL_URL = "https://github.com/10acad-group3/speech_recognition/tree/main/models/model.pkl"
 
+def tranlate_audio(path_audio_file):
+    # Dummy function
+    return "This is dummy translation: ሰላም ፣ ይህ የናሙና ትርጉም ብቻ ነው ፣ እሱ እውነተኛ አይደለም"
 
 st.set_page_config(page_title="Dashboard | Amharic Speech Recognition ", layout="wide")
 st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Amharic Speech Recognition </h1>", unsafe_allow_html=True)
@@ -51,20 +54,34 @@ def main():
         ### Select one of the following Amharic recorded Speech
         """)
 
-        st.audio(path_to_wave_files)
-        if st.button('Translate'):
-            # Function to translate
-            st.text_area('Hit Translate to get Amharic translations texts.. ')
+        try:
+            st.audio(path_to_wave_files)
+            if st.button('Click Here to Translate'):
+                # Function to translate
+                txt = tranlate_audio("path_audio_file")
+                st.text(txt)
+
+        except Exception as e:
+            pass
+
 
         st.write("""
         ### Upload Amharic audio file
         """)
 
-        file = st.file_uploader("Pick a file")
-        if st.button('Translate button'):
-            # Function to translate
+        try:
+            file = st.file_uploader("Pick a file")
+            if file != None:
+                if st.button('Click Here to Translate your Audio'):
+                    # Function to translate
+                    txt = tranlate_audio("path_audio_file")
+                    st.text(txt)
 
-            st.text_area('Hit Translate to get Amharic translations texts')
+            else:
+                st.error('Please, Upload the audio file')
+
+        except Exception as e:
+            pass
 
     elif transaltion_mode == real_time_translation:
         pass
