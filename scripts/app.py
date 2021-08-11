c69117c9623906f7aa1db7c8969d878cbdbd6346
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-11 17:24:27 +0300

    Added side bar with selection buttons

--- scripts/app.py
+++ scripts/app.py
@@ -18,5 +18,28 @@ import warnings
 import logging
 import logging.handlers
 
+path_to_wave_files = "../speech_recognition/data/AMHARIC/test/wav/01_d501021.wav"
+
 st.set_page_config(page_title="Dashboard | Amharic Speech Recognition ", layout="wide")
+
 st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Amharic Speech Recognition </h1>", unsafe_allow_html=True)
+
+dynamic_range = st.sidebar.write("""
+## Translate  Amharic Speech
+###
+""")
+
+st.sidebar.button('Load audio file')
+st.sidebar.button('Real time Translate')
+
+st.write("""
+## Select one of the following Amharic recorded Speech
+""")
+
+st.audio(path_to_wave_files)
+
+st.write("""
+## Upload Amharic audio file
+""")
+
+file = st.file_uploader("Pick a file")
