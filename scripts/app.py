3b8e9fbb50df8c125636fc959092571d014bc1a3
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-11 16:23:43 +0300

    Importing necessary packages and setting logging

--- scripts/app.py
+++ scripts/app.py
@@ -1,3 +1,17 @@
+# Importing packages
+import numpy as np
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
 import streamlit as st
 
-st.title('Speech Recognition Dashboard')
+# Setting Logs
+import warnings
+
+# Import packages for logging
+import logging
+import logging.handlers
+
+st.set_page_config(page_title="Dashboard | Speech Recognition Dashboard ", layout="wide")
+st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Speech Recognition Dashboard </h1>", unsafe_allow_html=True)
