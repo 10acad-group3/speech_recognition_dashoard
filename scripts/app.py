b73bd463bbaff6468614a1fa3c426fa3249e52f4
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-11 16:35:43 +0300

    Imported other packages and updated the title

--- scripts/app.py
+++ scripts/app.py
@@ -4,6 +4,11 @@ import pandas as pd
 import altair as alt
 import plotly.express as px
 
+import altair as alt
+import plotly.express as px
+import pickle
+import math
+
 import streamlit as st
 
 # Setting Logs
@@ -13,5 +18,5 @@ import warnings
 import logging
 import logging.handlers
 
-st.set_page_config(page_title="Dashboard | Speech Recognition Dashboard ", layout="wide")
-st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Speech Recognition Dashboard </h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Dashboard | Amharic Speech Recognition ", layout="wide")
+st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Amharic Speech Recognition </h1>", unsafe_allow_html=True)
