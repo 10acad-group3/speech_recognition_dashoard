c6bc2f62b96f338b0a9b50abdbb45c3fa17077f3
Elias <eandualem@gmail.com>
2021-08-12 17:17:18 +0300

    feat: add home page

---
+++ pages/home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def app():
+    # st.image('./img/rossman_store.jpeg')
+    st.write(
+        """
+        #### To try it out,  
+        - Please load the sample Amharic speech/audio files provided,
+        - Upload your Amharic audio file, or
+        - Use this demo to record your Speech and translate it
+    """)
