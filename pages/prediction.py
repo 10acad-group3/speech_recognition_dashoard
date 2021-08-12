913325e28ad6244b21a2ada51218a94ce4e09a7a
Elias <eandualem@gmail.com>
2021-08-12 17:28:19 +0300

    feat: add prediction page

---
+++ pages/prediction.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import streamlit as st
+import tensorflow as tf
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pickle
+sys.path.append(os.path.abspath(os.path.join('./scripts')))
+from predict import Predict
+
+
+def tranlate_audio(path_audio_file):
+    # Dummy function
+    return "This is dummy translation: ሰላም ፣ ይህ የናሙና ትርጉም ብቻ ነው ፣ እሱ እውነተኛ አይደለም"
+
+
+def app():
+    st.write(""" ### Upload Amharic audio file """)
+    pridict = Predict()
+
+    try:
+        file = st.file_uploader("Pick a file")
+        if file != None:
+            if st.button('Click Here to Translate your Audio'):
+                # Function to translate
+                txt = tranlate_audio("path_audio_file")
+                st.text(txt)
+
+        else:
+            st.error('Please, Upload the audio file')
+
+    except Exception as e:
+        print(f" Exception occurred in uploading audio file, {e}")
